8ad40a1a83960d6e9d2e83c1515641c1e69cc2a9
anastasia.test <nastyona957@yandex.ru>
2025-07-11 15:12:43 +0300

    formatting the code

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
         int priceTicket = 13676;
-
         int miles = priceTicket / 20;
-
         System.out.println("Вам начислено " + miles + " бонуса");
-
     }
 }
